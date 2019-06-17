650d5b3afa547207a919bda6d0f690c842f5801d
Matthew Foreman <matthew_foreman@gemvision.com>
2019-06-17 16:56:23 -0500

    boto3 might be needed. upload.py started for upload to s3 bucket.

---
+++ serverless/upload.py
@@ -0,0 +1,13 @@
+import boto3
+def main(event, context):
+    """
+    upload content to an s3 bucket.
+    :param event:
+    :param context:
+    :return: successful or unsucceful http response
+    """
+    if True:
+        return {
+            "statusCode": 200,
+            "body": {"message": "success"}
+        }
