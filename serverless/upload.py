e80632b0b04198acf1c950ed1b8ea0dcb745bae9
Matthew Foreman <matthew_foreman@gemvision.com>
2019-06-18 09:13:15 -0500

    basic upload file written with dummy bucket and object_name successful error message

--- serverless/upload.py
+++ serverless/upload.py
@@ -1,13 +1,47 @@
+import io
+import logging
 import boto3
+from botocore.exceptions import ClientError
+
+def upload_fileobj(file_obj, bucket, object_name):
+    """Upload a file to an S3 bucket
+
+    :param file_obj: File object to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name.
+    :return: True if file was uploaded, else False
+    """
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_fileobj(file_obj, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+
 def main(event, context):
     """
     upload content to an s3 bucket.
     :param event:
     :param context:
-    :return: successful or unsucceful http response
+    :return: successful or unsuccessful http response
     """
-    if True:
+    file_obj = io.BytesIO(b"test")
+    bucket = 'not.implemented'
+    object_name = '/also/not/implemented'
+    upload_occured = upload_fileobj(file_obj, bucket, object_name)
+    if upload_occured:
         return {
             "statusCode": 200,
             "body": {"message": "success"}
         }
+    else:
+        return {
+            "statusCode": 500,
+            "body": {"message": "InternalServerError: check AWS logging"}
+        }
+
